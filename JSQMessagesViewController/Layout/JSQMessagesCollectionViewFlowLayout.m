6f52dd5c0fcab1d30438b373df53cf262548efbb
Nikolay Kagala <nick@sequenia.com>
2016-06-16 14:16:10 +0600

    Change right/left section insets of the flow layout

--- JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
+++ JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
@@ -61,7 +61,7 @@ const CGFloat kJSQMessagesCollectionViewAvatarSizeDefault = 30.0f;
 - (void)jsq_configureFlowLayout
 {
     self.scrollDirection = UICollectionViewScrollDirectionVertical;
-    self.sectionInset = UIEdgeInsetsMake(10.0f, 4.0f, 10.0f, 4.0f);
+    self.sectionInset = UIEdgeInsetsMake(10.0f, 10.0f, 10.0f, 10.0f);
     self.minimumLineSpacing = 4.0f;
     
     _messageBubbleFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
